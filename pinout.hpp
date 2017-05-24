16f48f46cc1de42d2eb802c244b75353e3c5cc06
Jérémy Cheynet <jerem.cheynet@gmail.com>
2017-05-24 19:52:50 +0200

    feat: add pinout of board
    Signed-off-by: Jérémy Cheynet <jerem.cheynet@gmail.com>

---
+++ pinout.hpp
@@ -0,0 +1,17 @@
+#ifndef __PINOUT_H__
+#define __PINOUT_H__
+
+#define AHEAD_SENSOR A5
+#define BEHIND_SENSOR A4
+#define RIGHT_SENSOR A1
+#define LEFT_SENSOR A0
+
+#define RIGHT_MOTOR_PWM 10
+#define LEFT_MOTOR_PWM 9
+
+#define AHEAD_RIGHT_RELAY 7
+#define AHEAD_LEFT_RELAY 5
+#define BEHIND_RIGHT_RELAY 8
+#define BEHIND_LEFT_RELAY 6
+
+#endif
