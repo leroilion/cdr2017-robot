ef2c39d9834380c557571553422adb38fdf4ece6
Jérémy Cheynet <jerem.cheynet@gmail.com>
2017-05-24 19:53:02 +0200

    feat: add motor driver (only init and direction for now)
    Signed-off-by: Jérémy Cheynet <jerem.cheynet@gmail.com>

---
+++ motor.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include "motor.hpp"
+#include "pinout.hpp"
+
+void motorInit()
+{
+    pinMode(RIGHT_MOTOR_PWM , OUTPUT);
+    pinMode(LEFT_MOTOR_PWM, OUTPUT);
+    pinMode(AHEAD_RIGHT_RELAY, OUTPUT);
+    pinMode(AHEAD_LEFT_RELAY, OUTPUT);
+    pinMode(BEHIND_RIGHT_RELAY, OUTPUT);
+    pinMode(BEHIND_LEFT_RELAY, OUTPUT);
+}
+
+void setDirection(motorDirection direction)
+{
+    switch(direction)
+    {
+        case FRONT:
+            digitalWrite(AHEAD_RIGHT_RELAY, HIGH);
+            digitalWrite(AHEAD_LEFT_RELAY, HIGH);
+            digitalWrite(BEHIND_RIGHT_RELAY, LOW);
+            digitalWrite(BEHIND_LEFT_RELAY, LOW);
+            break;
+        case BACK:
+            digitalWrite(AHEAD_RIGHT_RELAY, LOW);
+            digitalWrite(AHEAD_LEFT_RELAY, LOW);
+            digitalWrite(BEHIND_RIGHT_RELAY, HIGH);
+            digitalWrite(BEHIND_LEFT_RELAY, HIGH);
+            break;
+        case RIGHT:
+            digitalWrite(AHEAD_RIGHT_RELAY, LOW);
+            digitalWrite(AHEAD_LEFT_RELAY, HIGH);
+            digitalWrite(BEHIND_RIGHT_RELAY, HIGH);
+            digitalWrite(BEHIND_LEFT_RELAY, LOW);
+            break;
+        case LEFT:
+            digitalWrite(AHEAD_RIGHT_RELAY, HIGH);
+            digitalWrite(AHEAD_LEFT_RELAY, LOW);
+            digitalWrite(BEHIND_RIGHT_RELAY, LOW);
+            digitalWrite(BEHIND_LEFT_RELAY, HIGH);
+            break;
+        case NONE:
+            digitalWrite(AHEAD_RIGHT_RELAY, LOW);
+            digitalWrite(AHEAD_LEFT_RELAY, LOW);
+            digitalWrite(BEHIND_RIGHT_RELAY, LOW);
+            digitalWrite(BEHIND_LEFT_RELAY, LOW);
+            break;
+    }
+}
